c01dd116605cae442580237fcd3778e1d529daa9
Potsky <potsky@me.com>
2016-09-29 12:30:52 +0200

    fix support for tests in PHP5.5

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,7 +3,7 @@
 class TestCase extends Orchestra\Testbench\TestCase
 {
 	const MOCK_DIR_PATH           = 'tests/mock';
-	const MOCK_DIR_PATH_GLOBAL    = self::MOCK_DIR_PATH . '/global';
+	const MOCK_DIR_PATH_GLOBAL    = 'tests/mock/global';
 	const MOCK_DIR_PATH_WO_LEMMA  = 'tests/mock/wo_lemma';
 	const LANG_DIR_PATH           = 'tests/lang';
 	const ORCHESTRA_LANG_DIR_PATH = 'vendor/orchestra/testbench/fixture/resources/lang';
